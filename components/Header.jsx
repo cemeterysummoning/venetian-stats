2d180dc34157097e9e0da22f01868633ebfd2fcd
cemeterysummoning <yunac477@gmail.com>
2024-07-02 12:45:31 -0400

    fixed links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,25 @@
 import BootstrapClient from "./BootstrapClient";
 import Image from "next/image";
 import logoImg from '../public/logo.png'
+import Link from "next/link";
 
 function Header() {
     return ( 
         <header>
-      <div class="navbar shadow-sm" style={{
+      <div className="navbar shadow-sm" style={{
         backgroundColor: "#FFFFFF"
       }}>
-        <div class="container d-flex justify-content-between">
-          <a href="/" class="navbar-brand d-flex align-items-center">
+        <div className="container d-flex justify-content-between">
+          <Link href="/" className="navbar-brand d-flex align-items-center">
           <Image src={logoImg} width="100" height="100" style={
             {
               padding: 10,
             }
           }/>
-          </a>
-          <a href="/" class="navbar-brand d-flex align-items-center">
+          </Link>
+          <Link href="/" className="navbar-brand d-flex align-items-center">
           <h4>Venetian Statistics</h4>
-          </a>
+          </Link>
         </div>
       </div>
     </header>
